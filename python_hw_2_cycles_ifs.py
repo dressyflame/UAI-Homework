de03fcbaa018ae5ac3831d682fc8118747702825
dressyflame <77794384+dressyflame@users.noreply.github.com>
2022-05-17 09:13:19 +0300

    Update python_hw_2_cycles_ifs.py

--- python_hw_2_cycles_ifs.py
+++ python_hw_2_cycles_ifs.py
@@ -1,6 +1,8 @@
+#Задача 1
 for i in range(5):
     print(f'%i 0000' % i)
-    
+
+#Задача 2    
 s = 0
 for i in range(10):
     n = int(input())
@@ -8,16 +10,19 @@ for i in range(10):
         s += 1
 print(s)
 
+#Задача 3
 s = 0
 for i in range(1, 101):
     s += i 
 print(s)
 
+#Задача 4
 p = 1
 for i in range(2, 11):
     p *= i
 print(p)
 
+#Задача 5
 n = str(int(input()))
 for l in n:
     print(l)
